4fecb21227164cb38b848157652a87fd3a922e85
Kirill <kirill040605@gmail.com>
2025-03-04 14:26:29 -0800

    todolist instance created

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"projects.com/todo/task"
+	"projects.com/todo/todolist"
+)
+
+func main() {
+	var todoList todolist.TodoList = todolist.TodoList{Tasks: []task.Task{}}
+
+}
